54113b90b53f8a1ae4539ca0a8251325581a05c6
hitansh98 <hitansh98@yahoo.com>
2022-04-11 18:28:30 +0530

    Time: 3 ms (98.84%), Space: 43 MB (87.98%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -6,10 +6,10 @@ class Solution {
         int[][] test = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int x = (i*n + j + k)%(m*n);
-                int x1 = x / n;
-                int y1 = x % n;
-                test[x1][y1] = grid[i][j];
+                // int x = (i*n + j + k)%(m*n);
+                // int x1 = x / n;
+                // int y1 = x % n;
+                test[((i*n + j + k)%(m*n)) / n][((i*n + j + k)%(m*n)) % n] = grid[i][j];
             }
         }
         
