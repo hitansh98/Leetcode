a6ac538e387e5db5b75264a782e412231cfdbb6d
hitansh98 <hitansh98@yahoo.com>
2022-04-11 18:27:13 +0530

    Time: 5 ms (92.64%), Space: 54.2 MB (75.58%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -3,16 +3,10 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        List<List<Integer>> al = new ArrayList<List<Integer>>(m); 
-        for(int i=0;i<m;i++){
-            List<Integer> internal = new ArrayList<Integer>(n);
-            al.add(i,internal);
-        }
         int[][] test = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int x = (i*n + j + k)%(m*n);
-                
                 int x1 = x / n;
                 int y1 = x % n;
                 
