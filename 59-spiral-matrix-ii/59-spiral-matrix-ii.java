ed772a5339d42a55d4ad30e6728a8c360f068564
hitansh98 <hitansh98@yahoo.com>
2022-04-13 22:13:39 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (85.14%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -1,50 +1,47 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-        int[][] mat = new int[n][n];
+        int[][] mat = new int[n][n]; //array will automatically initiate to 0's -- no need to maintain visited matrix. 
         
+        //direction matrix. We access both these matrix using the same temp variable at a time.
         int[] dx = new int[]{0,1,0,-1};
         int[] dy = new int[]{1,0,-1,0};
         
         int count = 1;
-        int temp = 0;
+        int temp = 0; //variable used to change direction.
         
+        //Current Position Variables.
         int currx = 0;
         int curry = 0;
         
+        //count==n*n condition because count variable is starting with 1.
         while(count<=n*n){
             
             mat[currx][curry] = count;
             count++;
             
+            //Potential next position variables to evaluate travel/ no travel.
             int nex = currx + dx[temp%4];
             int ney = curry + dy[temp%4];
-            // System.out.println(nex+" "+ney);
+            
+            //if we encounter conditions to not travel, simply change direction + modify new position variables. 
+            //Call user-defined fn for Out of bounds condition in the first operand of the OR condition to avoid runtime error.
             if(isArrayOOB(mat, nex, ney) || mat[nex][ney] != 0){
-                // System.out.println("change direction from: " +dx[temp%4]+" "+dy[temp%4]);
                 temp++;
-                // System.out.println("change direction to: " +dx[temp%4]+" "+dy[temp%4]);
                 nex = currx + dx[temp%4];
                 ney = curry + dy[temp%4];
             }
             
+            //Update current position variables for next iteration
             currx = nex;
             curry = ney;
         
         }
         
-        // System.out.println(temp+" "+count);
-        
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         System.out.print(mat[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        
         return mat;
         
     }
     
+    //User-defined fn for avoiding Out of Bounds condition.
     public boolean isArrayOOB(int[][] mat, int i, int j){
         return (i<0 || j<0 || i>=mat.length || j>=mat.length);
     }
