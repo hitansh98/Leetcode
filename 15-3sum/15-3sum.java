f663967822d3ef6fe8c8d4cc8b6eeeed267ecac5
hitansh98 <hitansh98@yahoo.com>
2022-06-27 17:11:16 +0530

    Time: 23 ms (96.57%), Space: 46.1 MB (98.57%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -3,31 +3,28 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> resArray = new ArrayList<List<Integer>>();
         for(int i=0;i<nums.length-2;i++){
-            int res = nums[i]*(-1);
-            // System.out.print("Target Sum: "+res+" ");
-            int start = i+1;
-            int end = nums.length-1;
-            while(start<end){
-                int tempSum = nums[start] + nums[end];
-                // System.out.println("Temp Sum: "+tempSum);
-                if(tempSum < res){
-                    start++;
-                }
-                else if(tempSum > res){
-                    end--;
-                }
-                else{
-                    // System.out.println("Enetered l1");
-                    List<Integer> lArr = new ArrayList<Integer>();
-                    lArr.add(nums[i]);
-                    lArr.add(nums[start]);
-                    lArr.add(nums[end]);
-                    if(!resArray.contains(lArr)){
-                        resArray.add(lArr);
+            if(i==0 || nums[i] != nums[i-1]){
+                int res = 0-nums[i];
+                int start = i+1;
+                int end = nums.length-1;
+                while(start<end){
+
+                    int tempSum = nums[start] + nums[end];
+                    if(tempSum < res){
+                        start++;
                     }
-                    start++;
-                    end--;
-                    continue;
+                    else if(tempSum > res){
+                        end--;
+                    }
+                    else{
+                        resArray.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                        while(start<end && nums[start]==nums[start+1]) start++;
+                        while(start<end && nums[end]==nums[end-1]) end--;
+                        start++; end--;
+                        continue;
+                        // System.out.println("Enetered l1");
+
+                    }
                 }
             }
             // System.out.println(resArray);
