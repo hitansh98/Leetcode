221ddef6d77135a2865a1fd66fadc7ee5e3393f5
hitansh98 <hitansh98@yahoo.com>
2022-06-28 15:58:21 +0530

    Time: 13 ms (57.63%), Space: 55.3 MB (73.31%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> hm = new HashMap<>();
+        if(strs.length==0) return new ArrayList<>();
         for(String str: strs){
             char[] cArr = str.toCharArray();
             Arrays.sort(cArr);
