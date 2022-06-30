848c5c352b3729649bb06b3cc5e1cbb81278ac44
hitansh98 <hitansh98@yahoo.com>
2022-06-30 20:46:47 +0530

    Time: 4 ms (27.87%), Space: 84.3 MB (23.57%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,35 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // if(prices.length <2){
-        //     return 0;
-        // }
-        int profitIfSoldToday = 0;
-        int maximumPossibleProfit = 0;
+        int max = 0;
+        int left = 0;
+        int right = 1;
         
-        int leastSoFar = Integer.MAX_VALUE;
-        
-        // while( right < prices.length){
-        //     if(prices[left] < prices[right]){
-        //         int profit = prices[right] - prices[left];
-        //         maxProfit = Math.max(maxProfit, profit);
-        //     }
-        //     else{
-        //         left = right;
-        //     }
-        //     right += 1;
-        // }
-        
-        for(int i=0;i<prices.length;i++){
-            if(leastSoFar > prices[i]){
-                leastSoFar = prices[i];
+        while(left<prices.length && right<prices.length){
+            int diff = prices[right]-prices[left];
+            
+            if(diff>max){
+                max = diff;
+                right++;
+                continue;
             }
             
-            profitIfSoldToday = prices[i]-leastSoFar;
-            if(maximumPossibleProfit < profitIfSoldToday ){
-                maximumPossibleProfit = profitIfSoldToday;
+            if(prices[right]<prices[left]){
+                left=right;
+                right++; 
+                continue;
             }
+            
+            right++;
+            
         }
         
-        return maximumPossibleProfit;
+        return max;
     }
 }
