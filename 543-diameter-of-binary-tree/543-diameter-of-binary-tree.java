7391a71c9b61ba978a321008a0c190a01d5936a0
hitansh98 <hitansh98@yahoo.com>
2022-07-07 08:21:54 +0530

    Time: 1 ms (62.76%), Space: 43.3 MB (48.16%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -14,21 +14,22 @@
  * }
  */
 class Solution {
-    int max = 0;
+    int max =0;
     public int diameterOfBinaryTree(TreeNode root) {
         maxDepth(root);
+        
         return max;
     }
     
     public int maxDepth(TreeNode root){
-        if(root == null){
+        if(root==null){
             return 0;
         }
         
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        
         max = Math.max(max, left+right);
-        return 1 + Math.max(left,right);
+        return 1+Math.max(left,right);
+        
     }
 }
