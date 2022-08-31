3642fa8aecfc21024db3d143f8085bd1ec44b5ad
hitansh98 <hitansh98@yahoo.com>
2022-08-31 19:13:05 +0530

    Time: 1 ms (76.45%), Space: 42.9 MB (25.17%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivotIndex = findPivot(nums);
+        if(pivotIndex==-1){
+            return binarySearch(nums, target, 0, nums.length-1);
+        }
+        
+        else if(nums[pivotIndex]==target){
+            return pivotIndex;
+        }
+        
+        int first = binarySearch(nums, target, 0, pivotIndex-1);
+        if(first!=-1){
+            return first;
+        }
+        
+        return binarySearch(nums, target, pivotIndex+1, nums.length-1);
+    }
+    
+    
+    public int findPivot(int[] nums){
+        int start= 0;
+        int end = nums.length-1;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(mid<nums.length-1 && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            
+            else if(mid>0 && nums[mid]<nums[mid-1]){
+                return mid-1;
+            }
+            
+            else if(nums[start] >= nums[mid]){
+                end = mid-1;
+            }
+            
+            else{
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    
+    public int binarySearch(int[] nums, int target, int start, int end){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            
+            else if(target>nums[mid]){
+                start = mid+1;
+            }
+            
+            else{
+                end = mid-1;
+            }
+            
+        }
+        return -1;
+    }
+}
