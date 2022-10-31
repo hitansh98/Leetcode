db7b63e18b3196468a1bbbaf4dbefdebc131c2a4
hitansh98 <hitansh98@yahoo.com>
2022-10-31 16:32:54 +0530

    Time: 2 ms (42.33%), Space: 45.6 MB (68.98%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean flag1 = true;
+        boolean flag2 = true;
+        for(int i=0;flag1 && flag2 && i<m;i++){
+            flag1 = check(matrix, i, 0, matrix[i][0]);
+            // System.out.println("flag1: "+flag1);
+        }
+        
+        for(int j=1;flag1 && flag2 && j<n;j++){
+            flag2 = check(matrix, 0, j, matrix[0][j]);
+            // System.out.println("flag2: "+flag2);
+        }
+        
+        return flag1 && flag2;
+    }
+    
+    public boolean check(int[][] arr, int i, int j, int val){
+        if(i>=arr.length || j>= arr[0].length){
+            return true;
+        } 
+        
+        return arr[i][j]==val && check(arr, i+1, j+1, val);
+    }
+}
