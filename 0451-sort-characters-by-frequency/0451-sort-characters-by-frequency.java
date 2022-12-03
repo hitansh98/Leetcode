5e6e39375d467b5891d89db369fead705dd0085e
hitansh98 <hitansh98@yahoo.com>
2022-12-04 04:41:24 +0530

    Time: 895 ms (17.67%), Space: 44.4 MB (82.73%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -8,15 +8,34 @@ class Solution {
         }
         
         
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue()-a.getValue());
-        pq.addAll(hm.entrySet());
+//         PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue()-a.getValue());
+//         pq.addAll(hm.entrySet());
         
+        
+        List<Character>[] pos = new List[s.length()+1];
+        
+        for(char key: hm.keySet()){
+            
+            if(pos[hm.get(key)] == null) pos[hm.get(key)] = new ArrayList<Character>();
+            pos[hm.get(key)].add(key);
+        }
         
         String res = "";
-        while(!pq.isEmpty()){
-            Map.Entry e = pq.poll();
-            for (int i = 0; i < (int)e.getValue(); i++) 
-                res += e.getKey();
+        // while(!pq.isEmpty()){
+        //     Map.Entry e = pq.poll();
+        //     for (int i = 0; i < (int)e.getValue(); i++) 
+        //         res += e.getKey();
+        // }
+        
+        for(int i=pos.length-1; i>=0;i--){
+            if(pos[i]==null) continue;
+            List<Character> list = pos[i];
+            for(int k=0;k<list.size();k++){
+                for(int j=0;j<i;j++){
+                    res += list.get(k);
+                }
+            }
+            
         }
         
         return res;
