f1f2130febde12c059fab3263efa74388f0d159c
hitansh98 <hitansh98@yahoo.com>
2022-12-24 15:13:49 +0530

    Time: 1 ms (91.36%), Space: 41.3 MB (73.51%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root==null) return res;
+        
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    q.offer(curr.right);
+                }
+                
+                if(i==size-1){
+                    res.add(curr.val);
+                }
+            }
+        }
+        
+        return res;
+        
+        
+        
+    }
+}
