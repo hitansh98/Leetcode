bbaa30c32253ffad056054f93f4c5f10ed05bc44
hitansh98 <hitansh98@yahoo.com>
2023-01-14 18:45:30 +0530

    Added solution - LeetHub

---
+++ Delete Middle of Linked List - GFG/delete-middle-of-linked-list.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            head = g.deleteMid(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+
+// } Driver Code Ends
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+        
+        if(head==null || head.next==null){
+            return null;
+        }
+        
+        Node ptr1 = head;
+        Node ptr2 = head;
+        
+        Node prev = null;
+        while(ptr2!=null && ptr2.next!=null){
+            prev = ptr1;
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next.next;
+        }
+        
+        prev.next = ptr1.next;
+        
+        return head;
+        
+    }
+}
