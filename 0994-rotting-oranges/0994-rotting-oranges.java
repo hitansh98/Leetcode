c8a9dd9994fdbd8f889c7140f51c2f6c0e3d7737
hitansh98 <hitansh98@yahoo.com>
2023-01-15 15:39:47 +0530

    Time: 2 ms (90.96%), Space: 42.1 MB (57.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,73 +1,47 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-        int[][] visited = new int[grid.length][grid[0].length];
-        boolean spoiled = false;
         int fresh = 0;
+        int time = 0;
+        LinkedList<int[]> q = new LinkedList<int[]>();
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2){
-                    q.offer(new Pair(i,j));
-                }
-                
                 if(grid[i][j]==1){
                     fresh++;
                 }
+                
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j});
+                }
             }
         }
         
+        int[][] dirs = new int[][]{{0,-1}, {0, 1}, {1, 0}, {-1,0}};
+        int[][] visited = new int[grid.length][grid[0].length];
         
-        if(fresh==0) return 0;
-        if(q.isEmpty()) return -1;
-        int[] xdirs = new int[]{-1,1,0,0};
-        int[] ydirs = new int[]{0,0,-1,1};
-        int time = 0;
         while(!q.isEmpty() && fresh>0){
             int size = q.size();
+            
             for(int i=0;i<size;i++){
-                Pair curr = q.poll();
-                for(int j=0;j<4;j++){
-                    int xcoord = curr.first + xdirs[j];
-                    int ycoord = curr.second + ydirs[j];
+                int[] curr = q.poll();
+                
+                for(int[] dir: dirs){
+                    int xcord = curr[0] + dir[0];
+                    int ycord = curr[1] + dir[1];
                     
-                    if(xcoord<0 || ycoord<0 || xcoord>=grid.length || ycoord>=grid[0].length || visited[xcoord][ycoord] == 1){
-                        continue;
-                    }
-                    if(grid[xcoord][ycoord] == 1){
-                        grid[xcoord][ycoord] = 2;
+                    if(xcord<0 || ycord<0 || xcord>=grid.length || ycord >=grid[0].length) continue;
+                    if(grid[xcord][ycord]==1 && visited[xcord][ycord]==0){
                         fresh--;
-                        q.offer(new Pair(xcoord, ycoord));
+                        q.offer(new int[]{xcord, ycord});
+                        grid[xcord][ycord] = 2;
                     }
-                    visited[xcoord][ycoord] = 1;
+                    
+                    visited[xcord][ycord] = 1;
                 }
             }
             time++;
-            // System.out.println(time+" "+q.toString());
         }
         
-        // for(int i=0;i<grid.length;i++){
-        //     for(int j=0;j<grid[0].length;j++){
-        //         System.out.print(grid[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        
-        
-        if(fresh>0){
-            return -1;
-        }
+        if(fresh>0) return -1;
         return time;
-    }
-    
-    
-}
-
-public class Pair{
-    int first;
-    int second;
-    
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
     }
 }
