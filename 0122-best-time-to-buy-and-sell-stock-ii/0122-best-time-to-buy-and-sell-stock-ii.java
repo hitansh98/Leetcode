fe7b3006dc1869ae22e14f240324dc361fb0b250
hitansh98 <hitansh98@yahoo.com>
2023-01-17 15:01:59 +0530

    Time: 1 ms (90.90%), Space: 42.3 MB (70.07%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,50 +1,37 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] ahead = new int[2];
-        int[] curr = new int[2];
-        ahead[0] = ahead[1] = 0;
-        // return helper(prices, 0, prices.length-1, dp);
         
+        if(prices.length<2) return 0;
         
-        for(int index = prices.length-1; index>=0;index--){
-            for(int buy = 0; buy<2; buy++){
-                if(buy==1){
-                    curr[buy] =  Math.max(-prices[index] + ahead[0], 0 + ahead[1]);
+        int[][] dp = new int[prices.length+1][2];
+        
+        dp[prices.length][0] = 0;
+        dp[prices.length][1] = 0;
+        
+        for(int i=prices.length-1;i>=0;i--){
+            for(int canBuy=0;canBuy<2;canBuy++){
+                if(canBuy==1){
+                    dp[i][canBuy] = Math.max(-prices[i] + dp[i+1][0], dp[i+1][1]);
                 }
                 else{
-                    curr[buy] =  Math.max(prices[index] + ahead[1], 0 + ahead[0]);    
+                    dp[i][canBuy] = Math.max(prices[i] + dp[i+1][1], dp[i+1][0]);
                 }
-                
-                ahead = curr;
             }
         }
         
-        
-        return ahead[1];
+        return dp[0][1];
     }
     
-    
-    public int helper(int[] prices, int index, int buy, int[][] dp){
-        //base case
+//     public int calcMaxProfit(int[] prices, int canBuy, int index){
+//         if(index>=prices.length){
+//             return 0;
+//         }
         
-        if(dp[index][buy]!=0){
-            return dp[index][buy];
-        }
-        
-        if(index<0){
-            return 0;
-        }
-        //if buy
-        if(buy==1){
-            dp[index][buy] =  Math.max(-prices[index] + dp[index+1][0], 0 + dp[index+1][1]);
-        }
-        
-        //if sell
-        else{
-            dp[index][buy] =  Math.max(prices[index] + dp[index+1][1], 0 + dp[index+1][0]);    
-        }
-        
-        return dp[index][buy];
-        
-    }
+//         if(canBuy==1){
+//             return Math.max(-prices[index] + calcMaxProfit(prices, 0, index+1), calcMaxProfit(prices, 1, index+1)); 
+//         }
+//         else{
+//             return Math.max(prices[index] + calcMaxProfit(prices, 1, index+1), calcMaxProfit(prices, 0, index+1));
+//         }
+//     }
 }
