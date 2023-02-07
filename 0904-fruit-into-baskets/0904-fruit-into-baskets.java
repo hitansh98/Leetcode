ba75d7df24f36ab7c1fe164e7073ef0010dfd180
hitansh98 <hitansh98@yahoo.com>
2023-02-07 17:46:20 +0530

    Time: 59 ms (41.66%), Space: 50 MB (95.57%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -21,31 +21,5 @@ class Solution {
         
         return res;
     }
-//     public int totalFruit(int[] tree) {
-//         Map<Integer, Integer> count = new HashMap<>();
-//         int i = 0, j;
-//         for (j = 0; j < tree.length; ++j) {
-//             count.put(tree[j], count.getOrDefault(tree[j], 0) + 1);
-//             if (count.size() > 2) {
-//                 count.put(tree[i], count.get(tree[i]) - 1);
-//                 count.remove(tree[i++], 0);
-//             }
-//         }
-//         return j - i;
-//     }
-    
-//     public int totalFruit2(int[] tree) {
-//         Map<Integer, Integer> count = new HashMap<Integer, Integer>();
-//         int res = 0, i = 0;
-//         for (int j = 0; j < tree.length; ++j) {
-//             count.put(tree[j], count.getOrDefault(tree[j], 0) + 1);
-//             while (count.size() > 2) {
-//                 count.put(tree[i], count.get(tree[i]) - 1);
-//                 if (count.get(tree[i]) == 0) count.remove(tree[i]);
-//                 i++;
-//             }
-//             res = Math.max(res, j - i + 1);
-//         }
-//         return res;
-//     }
+
 }
