65885262b6e110d2cf425cf8939beebbaf29a382
hitansh98 <hitansh98@yahoo.com>
2023-03-08 17:21:47 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (21.79%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,46 +1,51 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        int minVal = Integer.MAX_VALUE;
+        int minVal = 0;
+        int sum = 0;
         
         for(int num: nums){
-            minVal = Math.min(minVal, num);
+            sum += num;
+            minVal = Math.min(minVal, sum);
+            
         }
         // System.out.println(minVal);
 //         if(minVal>0){
             
 //         }
-        int low = 1;
-        int high = (-1*minVal*nums.length) + 1;
-        // System.out.println(high);
-        while(low<high){
-            int mid = (high-low)/2 + low;
+        
+        return 1-minVal;
+//         int low = 1;
+//         int high = (-1*minVal*nums.length) + 1;
+//         // System.out.println(high);
+//         while(low<high){
+//             int mid = (high-low)/2 + low;
             
-            if(!check(nums, mid)){
-                low = mid + 1; 
-            }
-            else{
-                high = mid;
-            }
-        }
+//             if(!check(nums, mid)){
+//                 low = mid + 1; 
+//             }
+//             else{
+//                 high = mid;
+//             }
+//         }
         
         
-        return low;
+//         return low;
     }
     
-    public boolean check(int[] arr, int mid){
-        int sum = 0;
-        // int sum=mid;
-        for(int i=0;i<arr.length;i++){
-            if(i==0){
-                sum += mid+arr[i];
-            }
-            else{
-                sum+=arr[i];
-            }
-            if(sum<1) return false;
-            // sum+=n;
-        }
+//     public boolean check(int[] arr, int mid){
+//         int sum = 0;
+//         // int sum=mid;
+//         for(int i=0;i<arr.length;i++){
+//             if(i==0){
+//                 sum += mid+arr[i];
+//             }
+//             else{
+//                 sum+=arr[i];
+//             }
+//             if(sum<1) return false;
+//             // sum+=n;
+//         }
         
-        return true;
-    }
+//         return true;
+//     }
 }
