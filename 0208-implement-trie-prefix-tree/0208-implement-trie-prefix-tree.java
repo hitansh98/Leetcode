3bcff00bfe8087920ec9ed51ee374df80384fa33
hitansh98 <hitansh98@yahoo.com>
2023-03-17 13:31:13 +0530

    Time: 58 ms (12.96%), Space: 59.4 MB (16.35%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,99 @@
+class Trie {
+    
+    HashMap<Character, Trie> hm;
+    public String toString(){
+        return this.hm.toString();
+    }
+    
+    public Trie() {
+        hm = new HashMap<>();
+    }
+    
+    public void insert(String word) {
+        Trie temp;
+        Trie curr = this;
+        for(int i=0;i<word.length();i++){
+            if(i==0){
+                if(curr.hm.containsKey(word.charAt(i))){
+                    temp = curr.hm.get(word.charAt(i));
+                }
+                else{
+                    temp = new Trie();
+                }
+                curr.hm.put(word.charAt(i), temp);
+                curr = temp;
+            }
+            else{
+                Trie temp2;
+                if(curr.hm.containsKey(word.charAt(i))){
+                    temp2 = curr.hm.get(word.charAt(i));
+                }
+                else{
+                    temp2 = new Trie();
+                }
+                curr.hm.put(word.charAt(i), temp2);
+                curr = temp2;
+            }
+            
+            if(i==word.length()-1){
+                curr.hm.put('#', null);
+            }
+        }
+        
+        // System.out.println(this);
+    }
+    
+    public boolean search(String word) {
+        Trie temp = this;
+        for(int i=0;i<word.length();i++){
+            if(temp == null) return false;
+            // System.out.println(temp.hm);
+            // System.out.println(word.charAt(i));
+            // System.out.println(temp.hm.containsKey(word.charAt(i)));
+            if(!temp.hm.containsKey(word.charAt(i))){
+                // System.out.println("I have reached end check 1");
+                return false;
+            }
+            // System.out.println(word.charAt(i));
+            if(i==word.length()-1){
+                // System.out.println(temp);
+                // System.out.println(temp.hm.get(word.charAt(i)).hm.keySet());
+                if(!temp.hm.get(word.charAt(i)).hm.containsKey('#')){
+                    // System.out.println("I have reached end check 2");
+                    return false;
+                }
+            }
+            else{
+                temp = temp.hm.get(word.charAt(i));
+            }
+        }
+        
+        return true;
+    }
+    
+    public boolean startsWith(String prefix) {
+        Trie temp = this;
+        
+        for(int i=0;i<prefix.length();i++){
+            if(temp == null) return false;
+            
+            if(!temp.hm.containsKey(prefix.charAt(i))){
+                return false;
+            }
+            
+            if(i != prefix.length()-1){
+                temp =temp.hm.get(prefix.charAt(i));
+            }
+            
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
