9d2a11088a1d15ccc8add6dec92359a76b17e247
hitansh98 <hitansh98@yahoo.com>
2023-03-18 20:44:51 +0530

    Time: 60 ms (46.56%), Space: 50.5 MB (53.18%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,63 @@
+class DoubleEndedQueue{
+    String val;
+    DoubleEndedQueue left;
+    DoubleEndedQueue right;
+    
+    public DoubleEndedQueue(String val, DoubleEndedQueue left, DoubleEndedQueue right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    
+    public DoubleEndedQueue(String val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+    
+}
+
+class BrowserHistory {
+    DoubleEndedQueue dqHead;
+    DoubleEndedQueue curr;
+    int size = 0;
+
+    public BrowserHistory(String homepage) {
+        this.dqHead = new DoubleEndedQueue(homepage);
+        this.curr = this.dqHead;
+        this.size = 1;
+    }
+    
+    public void visit(String url) {
+        DoubleEndedQueue toVisit =  new DoubleEndedQueue(url);
+        this.curr.right = toVisit;
+        toVisit.left = this.curr; 
+        this.curr = this.curr.right;
+        size++;
+    }
+    
+    public String back(int steps) {
+        while(this.curr.left!=null && steps-->0){
+            this.curr = this.curr.left;
+        }
+        
+        return this.curr.val;
+    }
+    
+    public String forward(int steps) {
+        
+        while(this.curr.right!=null && steps-->0){
+            this.curr = this.curr.right;
+        }
+        
+        return this.curr.val;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
