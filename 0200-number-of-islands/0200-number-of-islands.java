fb325838f957678bf71bac99aab91204adbf30b4
hitansh98 <hitansh98@yahoo.com>
2023-03-19 17:28:15 +0530

    Time: 10 ms (12.07%), Space: 50.8 MB (69.00%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,48 @@
 class Solution {
+    int res;
     public int numIslands(char[][] grid) {
-        int[][] visited = new int[grid.length][grid[0].length];
-        int res = 0;
+        res = 0;
+        char[][] visited = new char[grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(visited[i][j]==0 && grid[i][j]=='1'){
-                    dfs(grid, visited, i, j);
-                    res++;
-                }
+                if(grid[i][j] == '1' && visited[i][j] != '1'){
+                    // System.out.println(i+" "+j);
+                    bfs(grid, visited, i, j);   
+                    ++res;
+                }                
             }
         }
-         
-        return res;
         
+        
+        return res;
     }
     
-    public void dfs(char[][] grid, int[][] visited, int i, int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || visited[i][j]==1 || grid[i][j]=='0'){
-            return;
+    public void bfs(char[][] grid, char[][] visited, int i, int j){
+        // System.out.println(i+" "+j);
+        LinkedList<int[]> q = new LinkedList<>();
+        q.push(new int[]{i, j});
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            for(int m=0;m<size;m++){
+                int[] curr = q.poll();
+                if(curr[0]==grid.length-1 && curr[1] == grid[0].length-1){
+                    // System.out.println("reached");
+                }
+                if(curr[0] < 0 || curr[0] >= grid.length || curr[1] < 0 || curr[1] >= grid[0].length || visited[curr[0]][curr[1]] == '1' || grid[curr[0]][curr[1]] != '1'){
+                    continue;
+                } 
+                
+                
+                visited[curr[0]][curr[1]] = '1';
+                
+                q.offer(new int[]{curr[0] + 1, curr[1]});
+                q.offer(new int[]{curr[0] - 1, curr[1]});
+                q.offer(new int[]{curr[0], curr[1] + 1});
+                q.offer(new int[]{curr[0], curr[1] - 1});
+                
+            }
         }
         
-        visited[i][j]=1;
-        
-        dfs(grid, visited, i+1, j);
-        dfs(grid, visited, i-1, j);
-        dfs(grid, visited, i, j+1);
-        dfs(grid, visited, i, j-1);
     }
 }
