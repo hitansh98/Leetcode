d32c09609fab590c79817211fe047af1eb72e7fd
hitansh98 <hitansh98@yahoo.com>
2023-03-20 12:16:15 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (22.58%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,29 +1,23 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        // return false;
         
+        int[] bed = new int[flowerbed.length+2];
         
+        bed[0] = 0;
+        bed[bed.length-1] = 0;
         
-        
-        
-        
-        List<Integer> bed = new ArrayList<>();
-        
-        bed.add(0);
-        
+
         for(int i=0;i<flowerbed.length;i++){
-            bed.add(flowerbed[i]);
+            bed[i+1] = flowerbed[i];
         }
-        
-        bed.add(0);
         
         // System.out.println(bed);
         int count = 0;
         int result = 0;
         
-        for(int i=0;i<bed.size();i++){
+        for(int i=0;i<bed.length;i++){
             // System.out.println(i+" "+count);
-            if(bed.get(i) == 0){
+            if(bed[i] == 0){
                 count++;
             }
             else{
@@ -33,34 +27,7 @@ class Solution {
         }
         
         result+=(count-1)/2;
-            
-//         int left1 = 0;
-//         int right = 1;
-//         while(right<bed.size() && left1<bed.size()){
-//             // System.out.println(bed);
-//             while(right<bed.size() && left1<bed.size() && bed.get(right) != bed.get(left1)){
-//                 right++;
-//                 left1++;
-//             }
-//             int windowSize = right - left;
-//             while(windowSize-- > 0){
-//                 bed.remove(left);
-//             }
-//             left = right;
-//             left1 = right;
-//         }
-        
-//         System.out.println(bed);
-        
-//         int maxFlowers = 0;
-//         if(bed.size() % 2==0){
-//             maxFlowers = bed.size()/2;
-//         }
-//         else{
-//             maxFlowers += bed.size()%2 + bed.size()/2;
-//         }
-//         // System.out.println(maxFlowers);
-        // System.out.println(result);
+       
         
         return n<=result ? true : false;
     }
