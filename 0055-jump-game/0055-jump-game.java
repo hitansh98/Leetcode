067a69d63e8dca7f6ae06e118f430b9b8d49a217
hitansh98 <hitansh98@yahoo.com>
2023-03-22 01:01:46 +0530

    Time: 1 ms (99.99%), Space: 43 MB (41.49%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int flag = nums.length - 1;
-        int ptr = flag-1;
-        // System.out.println("initially ptr: " + ptr);
-        while(ptr>=0){
+        int ptr = flag;
+        
+        while(--ptr>=0){
             if(ptr + nums[ptr] >=flag){
                 flag = ptr;
-                // System.out.println("new flag: "+flag);
             }
-            ptr--;
         }
         
         return flag==0;
