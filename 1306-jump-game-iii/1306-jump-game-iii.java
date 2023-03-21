3b33f767ecd51097784121f249473170448d1f09
hitansh98 <hitansh98@yahoo.com>
2023-03-22 01:26:13 +0530

    Time: 116 ms (5.12%), Space: 49.9 MB (83.41%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,33 +1,56 @@
 class Solution {
-    Boolean[] dp;
+    // Boolean[] dp;
     boolean[] vis;
     
-    public boolean canReach(int[] arr, int start) {
-        dp = new Boolean[arr.length];
-        vis = new boolean[arr.length];
+    public boolean canReach(int[] nums, int start) {
+        // dp = new Boolean[arr.length];
+        vis = new boolean[nums.length];
         Arrays.fill(vis, false);
-        return helper(arr, start);
+        // return helper(arr, start);
+        // System.out.println(Arrays.toString(vis));
+        
+        LinkedList<Integer> q = new LinkedList<>();
+        
+        q.push(start);
+        while(!q.isEmpty()){
+            int size = q.size();
+            System.out.println(q);
+            for(int i=0;i<size;i++){
+                int curr = q.poll();
+                
+                if(nums[curr] == 0){
+                    return true;
+                }
+                
+                if(curr + nums[curr] < nums.length && vis[curr + nums[curr]] != true) q.offer(curr + nums[curr]);
+                if(curr - nums[curr] >= 0 && vis[curr - nums[curr]] != true) q.offer(curr - nums[curr]);
+                
+                vis[curr] = true;
+            }
+        }
+        
+        return false;
     }
     
-    public boolean helper(int[] arr, int start){
-        if(start<0 || start >= arr.length || vis[start] == true){
-            return false;
-        }
+//     public boolean helper(int[] arr, int start){
+//         if(start<0 || start >= arr.length || vis[start] == true){
+//             return false;
+//         }
         
-        if(arr[start] == 0){
-            return true;
-        }
+//         if(arr[start] == 0){
+//             return true;
+//         }
         
         
-        if(dp[start] != null){
-            return dp[start];
-        }
+//         if(dp[start] != null){
+//             return dp[start];
+//         }
         
-        vis[start] = true;
-        dp[start] = helper(arr, start + arr[start]) || helper(arr, start - arr[start]);
+//         vis[start] = true;
+//         dp[start] = helper(arr, start + arr[start]) || helper(arr, start - arr[start]);
         
-        return dp[start];
-    }
+//         return dp[start];
+    // }
     
     
 }
