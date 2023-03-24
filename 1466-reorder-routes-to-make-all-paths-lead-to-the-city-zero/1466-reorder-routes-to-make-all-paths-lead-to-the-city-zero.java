18de75f522d5d91c74515201cade23329c7e886c
hitansh98 <hitansh98@yahoo.com>
2023-03-24 15:32:02 +0530

    Time: 68 ms (64.21%), Space: 76.1 MB (40.66%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -3,40 +3,44 @@ class Solution {
     public int minReorder(int n, int[][] connections) {
         int res = 0;
         List<Integer>[] undirectedMap = new List[n];
-        
+        List<Integer>[] directedMap = new List[n];
         for(int i=0;i<undirectedMap.length;i++){
             undirectedMap[i] = new ArrayList<Integer>();
+            directedMap[i] = new ArrayList<Integer>();
         }
         
         for(int i=0;i<connections.length;i++){
             int[] curr = connections[i];
             undirectedMap[curr[0]].add(curr[1]);
-            undirectedMap[curr[1]].add(-curr[0]);
-            
+            undirectedMap[curr[1]].add(curr[0]);
+            directedMap[curr[0]].add(curr[1]);
         }
-        
         
         
         boolean[] vis = new boolean[n];
         Arrays.fill(vis, false);
         
         
-        return dfs(0, undirectedMap, vis);
+        return dfs(0, undirectedMap, directedMap, vis);
     }
     
     
     
-    public int dfs(int val, List<Integer>[] undirectedMap, boolean[] vis){
+    public int dfs(int val, List<Integer>[] undirectedMap, List<Integer>[] directedMap, boolean[] vis){
         List<Integer> conns = undirectedMap[val];
+        List<Integer> directedConns = directedMap[val];
+        
+        // System.out.println(conns);
+        // System.out.println(directedConns);
+        int changes = 0;
         vis[val] = true;
-        int change = 0;
         
         for(int conn: conns){
-            if(!vis[Math.abs(conn)]){
-                change += dfs(Math.abs(conn), undirectedMap, vis) + (conn > 0 ? 1 : 0);    
+            if(!vis[conn]){
+                changes += dfs(conn, undirectedMap, directedMap, vis) + (directedMap[val].contains(conn) ? 1 : 0);
             }
         }
         
-        return change;
+        return changes;
     }
 }
