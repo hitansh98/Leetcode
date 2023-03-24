8794ed54372e01cd186f5221af49eea37f807b4d
hitansh98 <hitansh98@yahoo.com>
2023-03-24 15:22:02 +0530

    Time: 41 ms (90.11%), Space: 64.6 MB (87.28%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -3,21 +3,18 @@ class Solution {
     public int minReorder(int n, int[][] connections) {
         int res = 0;
         List<Integer>[] undirectedMap = new List[n];
-        // List<Integer>[] directedMap = new List[n];
+        
         for(int i=0;i<undirectedMap.length;i++){
             undirectedMap[i] = new ArrayList<Integer>();
-            // directedMap[i] = new ArrayList<Integer>();
         }
         
         for(int i=0;i<connections.length;i++){
             int[] curr = connections[i];
             undirectedMap[curr[0]].add(curr[1]);
             undirectedMap[curr[1]].add(-curr[0]);
-            // directedMap[curr[0]].add(curr[1]);
+            
         }
         
-        // System.out.println(Arrays.toString(undirectedMap));
-        // System.out.println(Arrays.toString(directedMap));
         
         
         boolean[] vis = new boolean[n];
@@ -31,11 +28,6 @@ class Solution {
     
     public int dfs(int val, List<Integer>[] undirectedMap, boolean[] vis){
         List<Integer> conns = undirectedMap[val];
-        // List<Integer> directedConns = directedMap[val];
-        
-        // System.out.println(conns);
-        // System.out.println(directedConns);
-        
         vis[val] = true;
         int change = 0;
         
