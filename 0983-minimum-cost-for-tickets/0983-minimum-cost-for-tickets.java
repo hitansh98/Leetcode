b58d05a966e5c78775a046029c5136e5db10ff43
hitansh98 <hitansh98@yahoo.com>
2023-03-28 12:03:35 +0530

    Time: 3 ms (21.01%), Space: 43.2 MB (5.87%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -0,0 +1,27 @@
+class Solution {
+    Integer[][] dp;
+    public int mincostTickets(int[] days, int[] costs) {
+        dp = new Integer[days.length][425];
+        return helper(days, costs, 0, 0);
+    }
+    
+    public int helper(int[] days, int[] costs, int index, int validTill){
+        if(index >= days.length){
+            return 0;
+        }
+        
+        if(dp[index][validTill] != null){
+            return dp[index][validTill];
+        }
+        
+        if(validTill < days[index]){
+            int oneDay = costs[0] + helper(days, costs, index+1, days[index]);
+            int oneWeek = costs[1] + helper(days, costs, index+1, days[index]+6);
+            int oneMonth = costs[2] + helper(days, costs, index+1, days[index]+29);
+            return dp[index][validTill] = Math.min(Math.min(oneDay, oneWeek), oneMonth);
+        }
+        
+        return dp[index][validTill] = helper(days, costs, index+1, validTill);
+        
+    }
+}
