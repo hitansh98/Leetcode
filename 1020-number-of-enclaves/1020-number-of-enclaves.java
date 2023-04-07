761bfd9555268457d41dc3358ba37bb90abd00e5
hitansh98 <hitansh98@yahoo.com>
2023-04-07 11:50:07 +0530

    Time: 11 ms (46.65%), Space: 54.8 MB (56.55%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -3,22 +3,22 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         res = 0;
         
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        
         for(int i=0;i<grid.length;i++){
-            floodFill(grid, visited, i, 0);
-            floodFill(grid, visited, i, grid[0].length-1);
+            floodFill(grid, i, 0);
+            floodFill(grid, i, grid[0].length-1);
         }
         
         for(int j=0;j<grid[0].length;j++){
-            floodFill(grid, visited, 0, j);
-            floodFill(grid, visited, grid.length-1, j);
+            floodFill(grid, 0, j);
+            floodFill(grid, grid.length-1, j);
         }
         
         
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == 1){
-                    dfs(grid,visited, i, j);   
+                    dfs(grid, i, j);   
                 }
             }
         }
@@ -28,31 +28,31 @@ class Solution {
         
     }
     
-    public void floodFill(int[][] grid, boolean[][] visited, int i, int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || visited[i][j]==true || grid[i][j]==0){
+    public void floodFill(int[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
             return;
         }
         
-        grid[i][j] = 1;
-        visited[i][j] = true;
+        grid[i][j] = 0;
         
-        floodFill(grid, visited, i+1, j);
-        floodFill(grid, visited, i-1, j);
-        floodFill(grid, visited, i, j+1);
-        floodFill(grid, visited, i, j-1);
+        
+        floodFill(grid, i+1, j);
+        floodFill(grid, i-1, j);
+        floodFill(grid, i, j+1);
+        floodFill(grid, i, j-1);
     }
     
-    public void dfs(int[][] grid, boolean[][] visited, int i, int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || visited[i][j]==true || grid[i][j]==0){
+    public void dfs(int[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
             return;
         } 
         
         res++;
-        visited[i][j] = true;
+        grid[i][j] = 0;
         
-        dfs(grid, visited, i+1, j);
-        dfs(grid, visited, i-1, j);
-        dfs(grid, visited, i, j+1);
-        dfs(grid, visited, i, j-1);
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
     }
 }
