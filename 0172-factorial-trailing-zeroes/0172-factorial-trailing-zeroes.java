21841db0f569b4837111be93d50bf939554f983e
hitansh98 <hitansh98@yahoo.com>
2023-04-13 18:41:58 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (42.14%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,13 +1,32 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int num = n - n%5;
+        // O(n) sol
+//         int num = n - n%5;
+//         int count = 0;
+//         for(int i=5;i<=num;i+=5){
+//             int x = i;
+//             while(x%5==0){
+//                 count++;
+//                 x/=5;
+//             }
+//         }
+        
+        
+//         return count;
+        
+        
+        // O(log5n) sol
+        
+        if(n<5){
+            return 0;
+        }
+        
         int count = 0;
-        for(int i=5;i<=num;i+=5){
-            int x = i;
-            while(x%5==0){
-                count++;
-                x/=5;
-            }
+        int num = n- n%5;
+        
+        
+        for(int i=5;i<=num;i*=5){
+            count += num/i;
         }
         
         
