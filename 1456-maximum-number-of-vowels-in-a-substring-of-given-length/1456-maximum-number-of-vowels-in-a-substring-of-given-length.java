593f9309d953d165bdbb8e9ddae07d537e94f6f8
hitansh98 <hitansh98@yahoo.com>
2023-05-06 00:52:18 +0530

    Time: 9 ms (98.50%), Space: 43.3 MB (41.85%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -6,22 +6,23 @@ class Solution {
         vowels[8] = 1;
         vowels[14] = 1;
         vowels[20] = 1;
+        char[] sArr = s.toCharArray();
         
         int count = 0;
         int ans = 0;
         for(int i=0;i<k;i++){
-            if(vowels[s.charAt(i) - 'a'] == 1){
+            if(vowels[sArr[i] - 'a'] == 1){
                 count++;
             }
         }
         
         ans = Math.max(ans, count);
         
-        for(int i=k;i<s.length();i++){
-            if(vowels[s.charAt(i-k) - 'a'] == 1){
+        for(int i=k;i<sArr.length;i++){
+            if(vowels[sArr[i-k] - 'a'] == 1){
                 count--;
             }
-            if(vowels[s.charAt(i) - 'a'] == 1){
+            if(vowels[sArr[i] - 'a'] == 1){
                 count++;
             }
             
