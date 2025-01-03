dde43b8f00d53adf0003f90294520d25e8a645e5
hitansh98 <hitansh98@yahoo.com>
2025-01-03 23:00:32 +0530

    Time: 4 ms (16.81%), Space: 61.2 MB (14.07%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -2,25 +2,17 @@ class Solution {
     public int waysToSplitArray(int[] nums) {
         int n = nums.length;
         long[] leftSums = new long[n];
-        long[] rightSums = new long[n];
 
         for(int i=0;i<n;i++) {
             leftSums[i] += nums[i];
-            if(i > 1){
+            if(i != 0){
                 leftSums[i] += leftSums[i-1];
-                rightSums[n-i-1] += nums[n-i] + rightSums[n-i];
-            } else if(i == 1) {
-                leftSums[i] +=  leftSums[i-1];
-                rightSums[n-i-1] += nums[n-i] + rightSums[n-i];
-            }         
-            else {
-                rightSums[n-i-1] = 0; 
-            }
+            } 
         }
 
         int result = 0;
         for(int i=0;i<n-1;i++){
-            if(leftSums[i] >= rightSums[i]) {
+            if(leftSums[i] >= leftSums[n-1] - leftSums[i]) {
                 result++;
             }
         }
