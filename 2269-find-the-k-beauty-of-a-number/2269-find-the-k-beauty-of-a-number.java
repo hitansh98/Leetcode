f96fa8e3d47c01b708d2031ee58f40bdad966837
hitansh98 <hitansh98@yahoo.com>
2025-01-03 23:25:17 +0530

    Time: 1 ms (40.99%), Space: 40.6 MB (57.06%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -4,7 +4,6 @@ class Solution {
         int result = 0;
         for(int i=0;i<numString.length() - k + 1;i++) {
             String numSubs = numString.substring(i, i+k);
-            System.out.println(numSubs);
             if(Integer.valueOf(numSubs) != 0 && num % Integer.valueOf(numSubs) == 0) {
                 result++;
             }
