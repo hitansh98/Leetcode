9dd52d4591da09463d833b006fae40199de18209
hitansh98 <hitansh98@yahoo.com>
2025-05-11 10:54:43 +0530

    Time: 0 ms (100%), Space: 42.1 MB (85.27%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for(int num: arr) {
+            if(num % 2 != 0) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if(count == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
